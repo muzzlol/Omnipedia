b4d6366bf59cb2da077b6c4c970b3ee8437108c1
muzzlol <muzxmmilkhxn@gmail.com>
2024-10-11 19:21:07 +0530

    Created home page with navbar and serach bar import

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SearchBar } from '@/components/Searchbar';
+
+export const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Our Platform</h1>
+      <SearchBar />
+    </div>
+  );
+};
+

b9a2c7fe47550423e328190fb412c7e12162201e
muzzlol <muzxmmilkhxn@gmail.com>
2024-10-16 16:03:58 +0530

    changed text

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { SearchBar } from '@/components/Searchbar';
 export const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Our Platform</h1>
+      <h1 className="text-4xl font-bold mb-8">Omnipedia</h1>
       <SearchBar />
     </div>
   );

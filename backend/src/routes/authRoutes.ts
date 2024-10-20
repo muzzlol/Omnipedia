b0fbd538baaad277aa3e33fb80cab5a032688b9f
muzzlol <muzxmmilkhxn@gmail.com>
2024-10-20 19:41:45 +0530

    update

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { register, login } from '../controllers/authController';
+import asyncHandler from '../utils/asyncHandler';
 
 const router = express.Router();
 
-router.post('/register', register as any);
-router.post('/login', login as any);
+router.post('/register', asyncHandler(register));
+router.post('/login', asyncHandler(login));
 
 export default router;

0cb58d7d3553ec543ad4a5b530f2dd2675b02310
muzzlol <muzxmmilkhxn@gmail.com>
2024-11-05 23:59:11 +0530

    update

--- backend/src/config/redisConfig.ts
+++ backend/src/config/redisConfig.ts
@@ -1,4 +1,7 @@
 import { createClient, RedisClientType } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const redisURL = process.env.REDIS_URL;
 const redisClient = createClient({ url: redisURL }) as RedisClientType;
